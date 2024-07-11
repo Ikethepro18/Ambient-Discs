192ccb8e316a432543c0461af20e0dae93726333
Ikethepro18 <ikethepro18@gmail.com>
2024-07-11 21:07:21 +0100

    Base project files prepped

---
+++ src/main/java/ike/ambientdiscs/AmbientDiscsClient.java
@@ -0,0 +1,10 @@
+package ike.ambientdiscs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmbientDiscsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
