338c9655f8fc6acb3b93c5abc48364e9cd9b548e
Ikethepro18 <ikethepro18@gmail.com>
2024-07-17 12:43:02 +0100

    Fixed Aria Math cutting off

--- src/main/java/ike/ambientdiscs/sound/ModSounds.java
+++ src/main/java/ike/ambientdiscs/sound/ModSounds.java
@@ -15,6 +15,7 @@ public class ModSounds {
     public static final SoundEvent ARIA_MATH = registerSoundEvent("aria_math");
     public static final SoundEvent AXOLOTL = registerSoundEvent("axolotl");
 
+    //Registers the sound events
     private static SoundEvent registerSoundEvent(String name){
         Identifier id = Identifier.of(AmbientDiscs.MOD_ID, name);
         return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
